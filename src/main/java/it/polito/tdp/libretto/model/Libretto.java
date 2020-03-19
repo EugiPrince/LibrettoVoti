200c776f339d3e0949446a84b42d47c09f18f5cc
eugenioprincipi <eugenioprincipi@mbp-di-eugenio.lan>
2020-03-19 17:01:08 +0100

    Seconda parte dell'esercitazione

--- src/main/java/it/polito/tdp/libretto/model/Libretto.java
+++ src/main/java/it/polito/tdp/libretto/model/Libretto.java
@@ -17,9 +17,16 @@ public class Libretto {
 	 * Aggiunge un nuovo voto al libretto
 	 * 
 	 * @param v
+	 * @return {@code true} se ha inserito il voto, oppure {@code false} se era in conflitto o duplicato
 	 */
-	public void add(Voto v) {
-		this.voti.add(v);
+	public boolean add(Voto v) {
+		if(this.isConflitto(v) || this.isDuplicato(v)) { //In realta' si puo' chiamare solo il cercaNomeCorso..
+			return false;
+		}
+		else {
+			this.voti.add(v);
+			return true;
+		}
 	}
 	
 	/**
@@ -56,5 +63,63 @@ public class Libretto {
 		for(Voto v : this.voti)
 			s += v.toString()+"\n";
 		return s;
+	}
+
+	/**
+	 * Dato il nome di un corso ricerca se l'esame esiste nel libretto ed in tal caso restituisce
+	 * l'oggetto {@Link Voto} corrispondente
+	 * Se l'esame non esiste, restituisce null
+	 * 
+	 * @param nomeCorso nome esame da cercare
+	 * @return voto corrispondente oppure {@code null} (se non esiste)
+	 */
+	public Voto cercaNomeCorso(String nomeCorso) {
+		/*for(Voto v : this.voti)
+			if(nomeCorso.compareTo(v.getCorso())==0)
+				return v;
+		
+		return null;*/
+		
+		//Oppure uso indexOf (ma ho aggiungo equals in voto senno' non potrei)
+		int i = this.voti.indexOf(new Voto(nomeCorso, 0, null));//Voto e data a caso, mi creo un oggetto che non entra nel libretto
+		if(i!=-1)
+			return this.voti.get(i);
+		else
+			return null;
+	}
+	
+	/**
+	 * Ritorna true se il corso specificato da v e' gia' esistente
+	 * nel libretto, con la stessa valutazione. Se non esiste o la valutazione e' diversa
+	 * ritora false
+	 * 
+	 * @param v voto da ricercare
+	 * @return esistenza del duplicato
+	 */
+	public boolean isDuplicato(Voto v) {
+		Voto result = this.cercaNomeCorso(v.getCorso());
+		if(result==null)
+			return false;
+		
+		/*if(result.getVoto()==v.getVoto())
+			return true;
+		
+		return false;
+		*/
+		return (result.getVoto()==v.getVoto());
+	}
+	
+	/**
+	 * Determina se esiste un corso con lo stesso nome ma un voto diverso
+	 * 
+	 * @param v
+	 * @return
+	 */
+	public boolean isConflitto(Voto v) {
+		Voto result = this.cercaNomeCorso(v.getCorso());
+		if(result==null)
+			return false;
+		
+		return (result.getVoto()!=v.getVoto());
 	}
 }
--- src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/libretto/model/TestLibretto.java
@@ -9,18 +9,40 @@ public class TestLibretto {
 	private void run() {
 		this.lib = new Libretto(); //Crea un libretto vuoto
 		
+		
+		//1) Inserire alcuni voti
 		Voto v1 = new Voto("Tecniche di Programmazione", 30, LocalDate.of(2020, 06, 15));
 		Voto v2 = new Voto("Analisi II", 28, LocalDate.of(2020, 06, 28));
 		
 		lib.add(v1);
 		lib.add(v2);
-		lib.add(new Voto("Economia", 24, LocalDate.of(2020, 02, 14)));
+		if(lib.add(new Voto("Economia", 24, LocalDate.of(2020, 02, 14)))==false)
+				System.out.println("Errore nell'inserimento di Economia");
 		
 		System.out.println(this.lib);
 		
+		//2) Stampa tutti i voti pari a 28
 		System.out.println(this.lib.stampaVotiUguali(28));
 		
 		System.out.println(this.lib.estraiVotiUguali(28));
+		
+		//3) Cerca un esame superato, conoscendo il nome del corso
+		String nomeCorso = "Analisi II";
+		
+		Voto votoAnalisi = this.lib.cercaNomeCorso(nomeCorso);
+		System.out.println("Il voto di "+nomeCorso+" e' "+votoAnalisi.getVoto());
+		
+		//Voto mancante
+		Voto votoFisica = this.lib.cercaNomeCorso("Fisica I");
+		System.out.println("Il voto di "+nomeCorso+" e' "+votoFisica);
+		
+		//4) e 5) Verifica voti duplicati e voti in conflitto
+		Voto economia2 = new Voto("Economia", 24, LocalDate.now());
+		Voto economia3 = new Voto("Economia", 21, LocalDate.now());
+
+		System.out.println("Economia con 24 duplicato: "+lib.isDuplicato(economia2)+"/conflitto: "+lib.isConflitto(economia2));
+		System.out.println("Economia con 21 duplicato: "+lib.isDuplicato(economia3)+"/conflitto: "+lib.isConflitto(economia3));
+
 	}
 	
 	public static void main(String[] args) {
